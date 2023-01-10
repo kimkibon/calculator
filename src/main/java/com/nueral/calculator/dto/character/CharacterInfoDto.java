974b9fdf6a82f4f24192283176c5b248d6f90bfb
kimkibon <darkhalu@naver.com>
2023-01-10 21:48:35 +0900

    케릭터 인포메이션 스킬 / 굿즈 레벨 정렬

--- src/main/java/com/nueral/calculator/dto/character/CharacterInfoDto.java
+++ src/main/java/com/nueral/calculator/dto/character/CharacterInfoDto.java
@@ -5,9 +5,14 @@ import com.nueral.calculator.dto.goodsDto.GoodsCharacterDto;
 import com.nueral.calculator.dto.goodsDto.GoodsStatusCharacterDto;
 import com.nueral.calculator.dto.skillsDto.CharacterSkillsDto;
 import com.nueral.calculator.entity.Characters;
+import com.nueral.calculator.entity.algorithm.Algorithm;
 import com.nueral.calculator.entity.images.Skins;
+import com.nueral.calculator.entity.skill.AllSkills;
+import com.nueral.calculator.types.AlgorithmType;
+import com.nueral.calculator.types.SkillType;
 import lombok.*;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -38,9 +43,9 @@ public class CharacterInfoDto {
         this.companyType = characters.getCompanyType().getEnglish();
         this.defaultStar = characters.getDefaultStar();
         this.profile = characters.getProfile();
-        this.characterSkillsDtoList = characters.getAllSkillsList().stream().map(CharacterSkillsDto::new).collect(Collectors.toList());
-        this.characterAlgorithmDtoList = characters.getAlgorithmsList().stream().map(CharacterAlgorithmDto::new).collect(Collectors.toList());
-        this.goodsCharacterDtoList = characters.getGoodsCharacterList().stream().map(GoodsCharacterDto::new).collect(Collectors.toList());
+        this.characterSkillsDtoList = characters.getAllSkillsList().stream().sorted(Comparator.comparing(AllSkills::getSkillType)).map(CharacterSkillsDto::new).collect(Collectors.toList());
+        this.characterAlgorithmDtoList = characters.getAlgorithmsList().stream().sorted(Comparator.comparing(Algorithm::getAlgorithmType)).map(CharacterAlgorithmDto::new).collect(Collectors.toList());
+        this.goodsCharacterDtoList = characters.getGoodsCharacterList().stream().sorted((c1 , c2) -> c2.getAllGoods().getGoodsLevel() - c1.getAllGoods().getGoodsLevel()).map(GoodsCharacterDto::new).collect(Collectors.toList());
         this.goodsStatusCharactersDtoList = characters.getGoodsStatusCharactersList().stream().map(GoodsStatusCharacterDto::new).collect(Collectors.toList());
         this.skinDtoList = characters.getSkinsList().stream().map(SkinDto::new).collect(Collectors.toList());
     }
