38f762779f864d948a1581b84841a12cfbb73906
kimkibon <darkhalu@naver.com>
2023-01-24 00:30:25 +0900

    세트 알고리즘 입력/수정 페이지

--- src/main/java/com/nueral/calculator/controller/UpdateController.java
+++ src/main/java/com/nueral/calculator/controller/UpdateController.java
@@ -1,6 +1,10 @@
 package com.nueral.calculator.controller;
 
+import com.nueral.calculator.dto.AlgorithmDto.SetAlgorithmDtoList;
+import com.nueral.calculator.dto.ExpListDto.StarExpListDto;
 import com.nueral.calculator.dto.skillsDto.SkillEffectDto;
+import com.nueral.calculator.service.algorithm.AlgorithmService;
+import com.nueral.calculator.service.needExp.NeedStarExpService;
 import com.nueral.calculator.service.skill.SkillSaveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +16,10 @@ import org.springframework.web.bind.annotation.*;
 public class UpdateController {
     @Autowired
     SkillSaveService skillSaveService;
+    @Autowired
+    NeedStarExpService needStarExpService;
+    @Autowired
+    AlgorithmService algorithmService;
 
     @GetMapping(value = "/updateHome")
     public String updateHome(){
@@ -33,5 +41,27 @@ public class UpdateController {
     @PostMapping(value = "/deleteSkillEffect")
     public String deleteSkillEffects(@RequestParam("index") int index){
         return skillSaveService.deleteSkillEffect(index);
+    }
+
+    @GetMapping(value = "/updateStarExp")
+    public String updateStarExpPro(Model model){
+        model.addAttribute("starExp" , needStarExpService.updateStarExpPro());
+        return "update/updateStarExp";
+    }
+
+    @PostMapping(value = "/updateStarExp")
+    public String updateStarExp(@ModelAttribute StarExpListDto starExpListDto){
+        return needStarExpService.updateStarExp(starExpListDto);
+    }
+
+    @GetMapping(value = "/updateSetAlgorithm")
+    public String updateSetAlgorithmPro(Model model){
+        model.addAttribute("setAlgorithm" , new SetAlgorithmDtoList(algorithmService.setAlgorithmDtoList()));
+        return "update/updateSetAlgorithm";
+    }
+
+    @PostMapping(value = "/updateSetAlgorithm")
+    public String updateSetAlgorithm(@ModelAttribute SetAlgorithmDtoList setAlgorithmDtoList){
+        return algorithmService.saveSetAlgorithmList(setAlgorithmDtoList);
     }
 }
--- src/main/java/com/nueral/calculator/service/algorithm/AlgorithmService.java
+++ src/main/java/com/nueral/calculator/service/algorithm/AlgorithmService.java
@@ -2,6 +2,7 @@ package com.nueral.calculator.service.algorithm;
 
 import com.nueral.calculator.dto.AlgorithmDto.*;
 import com.nueral.calculator.entity.algorithm.Algorithm;
+import com.nueral.calculator.entity.algorithm.SetAlgorithm;
 import com.nueral.calculator.repository.CharacterRepository;
 import com.nueral.calculator.repository.algorithm.AlgorithmRepository;
 import com.nueral.calculator.repository.algorithm.MainAlgorithmRepository;
@@ -11,6 +12,7 @@ import com.nueral.calculator.types.AlgorithmType;
 import com.nueral.calculator.utils.FindTypes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -128,6 +130,25 @@ public class AlgorithmService {
         }
 
         return algorithmSaveDtolist;
+    }
+
+    @Transactional
+    public String saveSetAlgorithmList(SetAlgorithmDtoList setAlgorithmDtoList){
+        try {
+            for(SetAlgorithmDto dto : setAlgorithmDtoList.getSetAlgorithmList()){
+                SetAlgorithm setAlgorithm = SetAlgorithm.builder()
+                        .setAlgorithmName(dto.getSetAlgorithmName())
+                        .algorithmType(findTypes.findAlgorithmTypeByName(dto.getType()))
+                        .puckUpDay(dto.getPickUpDay())
+                        .twoSetOpt(dto.getTwoSetOpt())
+                        .threeSetOpt(dto.getThreeSetOpt())
+                        .build();
+                setAlgorithmRepository.save(setAlgorithm);
+            } return "home";
+        } catch (Exception e){
+            System.out.println("오류가 발생했습니다. : " + e.getMessage());
+            return "saveError";
+        }
     }
 
     public List<MainAlgorithmDto> mainAlgorithmDtoList(){
