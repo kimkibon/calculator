404950f3ef43c65979db59b91dc632f3cf2b62ea
kimkibon <darkhalu@naver.com>
2023-01-30 23:17:30 +0900

    프론트 페이지 수정중 추천 파티

--- src/main/java/com/nueral/calculator/entity/epParty/EpParty.java
+++ src/main/java/com/nueral/calculator/entity/epParty/EpParty.java
@@ -1,6 +1,7 @@
 package com.nueral.calculator.entity.epParty;
 
 import com.nueral.calculator.entity.DefaultEntity;
+import com.nueral.calculator.entity.character.Characters;
 import com.nueral.calculator.entity.epParty.id.EpPartyId;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,6 +20,7 @@ import java.util.List;
 @DynamicUpdate
 @DynamicInsert
 @IdClass(EpPartyId.class)
+@SequenceGenerator(name = "EP_PARTY_IDX_GENERATOR" , sequenceName = "EP_PARTY_SEQ", allocationSize = 1) // 시퀀스 생성
 public class EpParty extends DefaultEntity {
     @Id
     @ManyToOne(fetch = FetchType.LAZY,
@@ -27,15 +29,18 @@ public class EpParty extends DefaultEntity {
     @ToString.Exclude
     private EpPool epPool;
     @Id //PRIMARY_KEY
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "EP_PARTY_IDX_GENERATOR"
+    ) // 위에서 정의한 SEQ 를 사용하겠다.
     @Column(name = "EP_PARTY_INDEX")
     private int epPartyIndex;
-
     @OneToMany(mappedBy = "epParty" , fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @ToString.Exclude
     private List<EpMember> epMemberList;
 
     @Builder
-    public EpParty(EpPool epPool, int epPartyIndex){
+    public EpParty(EpPool epPool, int epPartyIndex ){
         this.epPool = epPool;
         this.epPartyIndex = epPartyIndex;
     }
