f2f3d41f5ee8b09bd413ce7e3e311c91edd2a8d0
kimkibon <darkhalu@naver.com>
2023-02-05 03:50:02 +0900

    고장협의 업데이트 완료 / 표기 고장협의 관련 수정 필요 가장 최근 1개만 표기

--- src/main/java/com/nueral/calculator/repository/epParty/EpPoolRepository.java
+++ src/main/java/com/nueral/calculator/repository/epParty/EpPoolRepository.java
@@ -4,5 +4,4 @@ import com.nueral.calculator.entity.epParty.EpPool;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EpPoolRepository extends JpaRepository<EpPool , Integer> {
-    long countByEpIndex(int epIndex);
 }
